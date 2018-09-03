ec2e9333be47ac9ca47d5aff58d506eddf3359c4
Thando-James <thandojames80@gmail.com>
2018-09-03 13:35:29 +0200

    latest

--- sdp_frontend/src/components/Scheduler.js
+++ sdp_frontend/src/components/Scheduler.js
@@ -7,33 +7,61 @@ class Students extends Component{
     constructor(props){
         super(props)
         this.onSubmit = this.onSubmit.bind(this)
+        this.onChecked = this.onChecked.bind(this)
+        this.getCourseStrings = this.getCourseStrings.bind(this)
         this.state={
             data:[],
-          }
+            checkedArr:[]
+        }
     }
 
     selectAll = function (){
         var checkboxes = document.getElementsByName('courses');
          for(var i=0, n=checkboxes.length;i<n;i++) {
           checkboxes[i].checked = true;
+          let e= {
+              target:{
+                  checked: true,
+                  value:checkboxes[i].value
+              }
+          }
+          this.onChecked(e)
         }
-    }
+    }.bind(this)
        
     deselectAll = function (){
         var checkboxes = document.getElementsByName('courses');
           for(var i=0, n=checkboxes.length;i<n;i++) {
             checkboxes[i].checked = false;
+            let e= {
+                target:{
+                    checked: false,
+                    value:checkboxes[i].value
+                }
             }
-    }
+            this.onChecked(e)
+            }
+    }.bind(this)
       
-    getCourseStrings = function(){
-        var c1;
-        var checked = Array.prototype.slice.call(document.querySelectorAll('.selectedcourses:checked')).map(function(c1){
-          return c1.value;
-        }).join(",");
+    getCourseStrings(){
+        let checked = this.state.checkedArr
+        let url = 'http://localhost:3456/generate'
         
-        console.log(checked);
+        fetch(url,{
+            method:"POST",
+            body:JSON.stringify({data:checked}),
+            headers: {
+                "Content-Type": "application/json; charset=utf-8",
+            },
+        })
+      .then(function(response){
+            console.log(response)
+        })
+        .catch(function(err){
+            console.log(err)
+        })
     }
+
 
     onSubmit(e){
         e.preventDefault();
@@ -57,44 +85,80 @@ class Students extends Component{
         })
     }
 
+    onChecked(e){
+        const checked = this.state.checkedArr
+        if(e.target.checked){
+            checked.push(e.target.value)
+        }else{
+            let index = checked.indexOf(e.target.value)
+            checked.splice(index,1)
+        }
+
+        this.setState({
+            checkedArr: checked
+        })
+    }
+
     render(){
         return(
             <div>
                 <PageHeader>
                     <h1>Timetable Scheduler</h1>
                 </PageHeader>
-
-                <ButtonToolbar >
-                <form onSubmit={this.onSubmit} id="students-form">
-                    <label>Upload a CSV with students registration data</label><br/><br/>
-                    <input type="file" name="file" />
-                    <p></p>
-                    <Button bsStyle="primary" className='btn' type="submit">Submit</Button>
-                </form>
-                </ButtonToolbar>
                 
-                <div align = "center">
-                
-                <div><Button  type="button" className="btn btn-primary"  onClick={this.selectAll}>Select All</Button></div>
-                <p></p>
-                <div><Button bsStyle="warning" onClick={this.deselectAll}>Deselect All</Button></div>
-                <p></p>
-                <div><Button bsStyle="success"onClick={this.getCourseStrings} >Generate</Button></div>
-                
-                </div>
-
-                <div>
-                    {this.state.data ? this.state.data.map((x)=>{
-                        console.log(x);
-                        return(
-                            <div>
-                                <input type="checkbox" name="courses" value={x} class = "selectedcourses"/> {x}
-                            </div>
-                        )}) : <div></div>
-                    }
+                <div class='row'>
+                    <div class='col-lg-5'>
+                        <h2 style={{textAlign:'left',marginLeft:'7%'}}>Courses</h2>
+                        <div className = "courses-list">
+                            {this.state.data ? this.state.data.map((x)=>{
+                                return(
+                                    <div>
+                                        <input type="checkbox" name="courses" value={x.course_code} class = "selectedcourses" onChange={this.onChecked}/> {x.course_code}
+                                    </div>
+                                )}) : <div></div>
+                            }
+                        </div>
+                    </div>
+                    <div class='col-lg-1' style={{height:2}}></div>
+                    <div align = "center" class='col-lg-4' style={{marginTop:'10%'}}>
+                        <div><Button  type="button" className="btn btn-primary"  onClick={this.selectAll}>Select All</Button></div>
+                        <p></p>
+                        <div><Button bsStyle="warning" onClick={this.deselectAll}>Deselect All</Button></div>
+                        <p></p>
+                        <div><Button bsStyle="success"onClick={this.getCourseStrings} >Generate</Button></div>
+                        <br/>
+                        <span>or</span>
+                        <br/><br/>
+                        <ButtonToolbar >
+                            <form onSubmit={this.onSubmit} id="students-form">
+                                <label>Upload a CSV with students registration data</label><br/><br/>
+                                <input type="file" name="file" />
+                                <br/>
+                                <Button bsStyle="primary" className='btn' style={{marginLeft:'42%'}} type="submit">Submit</Button>
+                            </form>
+                        </ButtonToolbar>
+                    </div>
                 </div>
             </div>
         )
+    }
+
+    componentDidMount(){
+        let _self = this;
+        let url = 'http://localhost:3456/display/courses'
+        fetch(url)
+        .then(function(res){
+            return res.json()
+        })
+        .then(function(response){
+            console.log(response)
+            _self.setState({
+                data:response
+            })
+        })
+        .catch(function(err){
+            console.log(err)
+        })
     }
 }
 
