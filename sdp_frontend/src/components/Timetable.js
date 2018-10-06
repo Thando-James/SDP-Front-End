48bcb8cf82f070cd0abff024c9018992ede0bc61
James Ndhlovu <james@naked.insure>
2018-10-06 22:58:26 +0200

    da

--- sdp_frontend/src/components/Timetable.js
+++ sdp_frontend/src/components/Timetable.js
@@ -5,15 +5,36 @@ import ReactHTMLTableToExcel from 'react-html-table-to-excel'
 import {Button} from 'react-bootstrap'
 import $ from 'jquery'
 import SelectSearch from 'react-select-search'
- let url = 'http://youthleague.co'
+import { EventCalendar} from 'react-event-calendar'
+ 
+let url = 'http://youthleague.co'
 //let url = 'http://localhost'
+
+const events = [
+    {
+        start: '2015-07-20',
+        end: '2015-07-02',
+        eventClasses: 'optionalEvent',
+        title: 'test event',
+        description: 'This is a test description of an event',
+    },
+    {
+        start: '2015-07-19',
+        end: '2015-07-25',
+        title: 'test event',
+        description: 'This is a test description of an event',
+        data: 'you can add what ever random data you may want to use later',
+    },
+];
 class Timetable extends Component{
+
     constructor(props){
         super(props)
         this.state={
           studentnumber:"",
           coursecode:"",
-          data:[]
+          data:[],
+          timetable: this.props.location.state
         }
     }
 
@@ -31,49 +52,48 @@ class Timetable extends Component{
             },
         })
         .then(function(response){
-          return response.json()
+            return response.json()
         })
         .then(function(response){
-          console.log(response)
-          _self.props.history.push({
-              pathname:'/stdtimetable',
-              state:response
-          })
-      })
-      .catch(function(err){
-          console.log(err)
-      })}.bind(this)
+            console.log(response)
+            _self.setState({
+                timetable:response
+            })
+        })
+        .catch(function(err){
+            console.log(err)
+        })
+    }.bind(this)
 
-      getNeighbor= function(){
+    getNeighbor= function(){
         let _self = this;
         var courseN = document.getElementById("courseN")
     
-       fetch(`${url}:3456/neighbors`,{
-          method:"POST",
-          body:JSON.stringify({coursecode:courseN.value
+        fetch(`${url}:3456/neighbors`,{
+            method:"POST",
+            body:JSON.stringify({coursecode:courseN.value
         }),
           headers: {
               "Content-Type": "application/json; charset=utf-8",
           },
-      })
-      .then(function(response){
-          return response.json()
-      })
-    .then(function(response){
-          console.log('Response from Nelly')
-          console.log(response)
-          _self.props.history.push({
-              pathname:'/interactions',
-              state:response
-          })
+        })
+        .then(function(response){
+            return response.json()
+        })
+        .then(function(response){
+            console.log('Response from Nelly')
+            console.log(response)
+            _self.props.history.push({
+                pathname:'/interactions',
+                state:response
+            })
         })
         .catch(function(err){
-          console.log(err)
+            console.log(err)
         })
     }.bind(this)
     
     search = function (){
-     
       $("#myInput").on("input",function(){
   
           var str = $('#myInput').val();
@@ -94,157 +114,150 @@ class Timetable extends Component{
           });
       });
   
-  }
-  render(){
+    }
 
-      
-    //   function search() {
-    //       var input, filter, table, tr, td, i,word;
-    //      input = document.getElementById("myInput");
-    //      filter = input.value.toUpperCase();
-    //      table = document.getElementById("sessions");
-    //     tr = table.getElementsByTagName("tr")
-    //       if(!(filter.includes(","))){
-    //         for (i = 0; i < tr.length; i++) {
-    //           td = tr[i].getElementsByTagName("td")[1];
-    //         console.log("This is td" + td)
-    //         console.log(test)
-    //         if (td) {
-    //           if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
-    //             tr[i].style.display = "";
-    //           } else {
-    //             tr[i].style.display = "none";
-    //            }
-    //          }       
-    //        }
-    //      }
-    //       else{
-    //       word = filter.split(",");
+    render(){
 
-    //       for(var j=0; j < word.length;j++){
-    //        for (i = 0; i < tr.length; i++) {
-    //           td = tr[i].getElementsByTagName("td")[1];
-              
-    //         console.log("This is td" + tr[i])
-    //         console.log(test)
-    //         if (td) {
-    //           if (td.innerHTML.toUpperCase().indexOf(word[j]) > -1) {
-    //             tr[i].style.display = "";
-    //           } else {
-    //             tr[i].style.display = "none"
-    //           }
-    //         }       
-    //       }
-    //     }
-    //   }
-    // }
-    return(
-    <div>
-    <pre> 
-     <PageHeader>
-        <h1 align='center'>Generated timetable with sessions</h1>
-         </PageHeader>
-         </pre>
-         <div className='row'>
+        // function search() {
+        //     var input, filter, table, tr, td, i,word;
+        //     input = document.getElementById("myInput");
+        //     filter = input.value.toUpperCase();
+        //     table = document.getElementById("sessions");
+        //     tr = table.getElementsByTagName("tr")
+        //     if(!(filter.includes(","))){
+        //         for (i = 0; i < tr.length; i++) {
+        //         td = tr[i].getElementsByTagName("td")[1];
+        //         console.log("This is td" + td)
+        //         console.log(test)
+        //         if (td) {
+        //         if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        //             tr[i].style.display = "";
+        //         } else {
+        //             tr[i].style.display = "none";
+        //         }
+        //         }       
+        //     }
+        //     }
+        //     else{
+        //     word = filter.split(",");
 
-        
-    <div class='col-lg-6' >
-            <div align='center'>
-             <ReactHTMLTableToExcel id="test" className="btn btn-primary" 
-            table="sessions" filename="Sessions table" sheet="sessions" buttonText="Download as XLS"/>
-           </div> 
-           <p></p>
+        //     for(var j=0; j < word.length;j++){
+        //     for (i = 0; i < tr.length; i++) {
+        //         td = tr[i].getElementsByTagName("td")[1];
+                
+        //         console.log("This is td" + tr[i])
+        //         console.log(test)
+        //         if (td) {
+        //         if (td.innerHTML.toUpperCase().indexOf(word[j]) > -1) {
+        //             tr[i].style.display = "";
+        //         } else {
+        //             tr[i].style.display = "none"
+        //         }
+        //         }       
+        //     }
+        //     }
+        // }
+        // }
+        return(
             <div>
-           <label style={{textAlign:'left',marginLeft:'12%'}}>Search for multiple courses by adding a ":" after course name</label> 
-           </div>
-           <div class ="form-group has-feedback has-search">
-           <span class="glyphicon glyphicon-search form-control-feedback" style={{marginRight:'30%'}}></span>
-           <input class="glyphicon glyphicon-search form-control-feedback" style={{marginLeft:'12%',width:'400px'}} type="text" id="myInput"  onKeyUp= {this.search}
-            placeholder="Search for courses.." title="Type a course" class="form-control"/>
-           </div><p></p>
-    <Table id ="sessions" align='center' bordered striped condensed hover style={{width:'500px'}} >
-                    <thead>                   
-                    <tr>
-                      <th>Sessions</th> 
-                      <th>Courses 
-                      </th>
-                    </tr>
-                    </thead>
-     
-     {this.props.location.state ? this.props.location.state.map((x)=>{
-                    console.log("X Structure:" + x);
-                   // for(var i=0;i<x.length;i++){
-                        //if()
-                      return(
-                            <tbody>
-                            <tr><td>{x.data[0]}</td>
-                            <td>{x.subject + " "}</td></tr>
-                            </tbody>
-                          )
-                   // }
-                        } ) : <div></div>
-                  }
-        
-           </Table>
-           </div>
-            <div className="col-lg-6" >
-            <label>Please enter student number:</label>
-            <div><input type="text" name="studentNum"  id = "stdNum" placeholder="Student number"/></div>
-            <p></p>
-            <Button bsStyle="success" onClick={this.sendStdnum}>Generate timetable</Button>
-            <p></p>
-            <span></span>
-            <p></p>
-            <label>Please enter course code:</label>
-            <div>
-            
-            <select id="courseN" >
-            <input type="text" placeholder="Search.." id="myInput" onkeyup="filterFunction()"/>
-           
-            {this.state.data != "" ? this.state.data.map((x)=>{
-              console.log(x.Course_Code)
-            return(
-             <option value={x.Course_Code}>{x.Course_Code}
-            </option>
-            )}) : <div></div>
-            }
-            </select>
-            </div>
-            <p></p>
-           <Button bsStyle="success" onClick={this.getNeighbor}>Check neighbors</Button>
-            </div>
-            </div>
-            </div>
-            
-            )
-          }
+                <pre> 
+                    <PageHeader>
+                        <h1 align='center'>Generated timetable with sessions</h1>
+                    </PageHeader>
+                </pre>
+                <div className='row'>
+                    <div class='col-lg-6' >
+                            <div align='center'>
+                                <ReactHTMLTableToExcel id="test" className="btn btn-primary" table="sessions" filename="Sessions table" sheet="sessions" buttonText="Download as XLS"/>
+                            </div> 
+                            <p></p>
+                            <div align='center'>
+                                <label>Search for multiple courses by adding a ":" after course name</label> 
+                            </div>
+                            <div class ="form-group has-feedback has-search" align='center'>
+                                <span class="glyphicon glyphicon-search form-control-feedback" style={{marginRight:'22%'}}></span>
+                                <input class="glyphicon glyphicon-search form-control-feedback" style={{width:'400px'}} type="text" id="myInput"  onKeyUp= {this.search} placeholder="Search for courses.." title="Type a course" class="form-control"/>
+                            </div>
+                            <p></p>
+                            <Table id ="sessions" align='center' bordered striped condensed hover style={{width:'500px'}} >
+                                <thead>                   
+                                    <tr>
+                                        <th>Sessions</th> 
+                                        <th>Courses</th>
+                                    </tr>
+                                </thead>
+                        
+                                {this.props.location.state? this.props.location.state.map((x)=>{
+                                    return(
+                                        <tbody>
+                                            <tr>
+                                                <td>{x.data[0]}</td>
+                                                <td>{x.subject + " "}</td>
+                                            </tr>
+                                        </tbody>
+                                    )} ) : <div></div>
+                                }
+                            
+                            </Table>
+                    </div>
+
+                    <div className="col-lg-6" >
+                        <div className="timetable row">
+                            <div className="col-lg-6">
+                                <input type="text" name="studentNum"  id = "stdNum" placeholder="Enter student number" style={{marginRight:10}}/>
+                                <br/>
+                                <br/>
+                                <Button bsStyle="success" onClick={this.sendStdnum} style={{float:'right', marginRight:60}}>Generate timetable</Button>
+                            </div>
+                           
+                           <div className="col-lg-6">
+                                <select style={{marginLeft:10}} >
+                                        <input type="text" placeholder="Search.." id="myInput" onkeyup="filterFunction()"/>
+                                    
+                                        {this.state.data != "" ? this.state.data.map((x)=>{
+                                            return(
+                                                <option value={x.Course_Code}>{x.Course_Code}</option>
+                                            )}) : <div></div>
+                                        }
+                                </select>
+                                <br/>
+                                <br/>
+                                <Button bsStyle="success" onClick={this.getNeighbor} style={{float:'right', marginRight:50}}>Check neighbors</Button>
+                           </div>
+                            
+                        </div>
+
+                        <EventCalendar 
+                            month={10}
+                            year={2018}
+                            events={events}
+                            // onEventClick={(target, eventData, day) => console.log(eventData)} 
+                        />
+                        
+                    </div>
+                </div>
+            </div>                
+        )
+    }
           
     componentDidMount(){
-      let _self = this;
-      fetch(`${url}:3456/display/courses`)
-      .then(function(res){
-          return res.json()
-      })
-      .then(function(response){
-          console.log(response)
-          _self.setState({
-              data:response
-          })
-      })
-      .catch(function(err){
-          console.log(err)
-      })
-    }}export default Timetable
-          
-          
-          
-            /*{this.props.location.state ? this.props.location.state.map((x)=>{
-             return(
-               
-              <div className="checklist" >
-              <input type="checkbox" name="courses" value={x}  onChange={this.onChecked}/> {x}
-              </div>
-             )}):<div></div>
-           } }*/
-         
+        let _self = this;
+        fetch(`${url}:3456/display/courses`)
+        .then(function(res){
+            return res.json()
+        })
+        .then(function(response){
+            console.log(response)
+            _self.setState({
+                data:response
+            })
+        })
+        .catch(function(err){
+            console.log(err)
+        })
+    }
+
+}
+    
+export default Timetable
   
